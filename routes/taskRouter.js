7cc62613e9522ffe5921910b9e328cb12a3362d1
kyrios12 <saahilhumayun@gmail.com>
2024-06-11 12:24:32 +0530

    Implemented Node Cache

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,27 +1,62 @@
-import {Router} from 'express';
-import {fetchTasks, getAddTask, postAddTask, getUpdateTask, patchUpdateTask, deleteTask} from '../controllers/taskController.js';
+import { Router } from 'express';
+import NodeCache from 'node-cache';
+import { fetchTasks, getAddTask, postAddTask, getUpdateTask, patchUpdateTask, deleteTask } from '../controllers/taskController.js';
 import verify from '../middleware.js';
-// let token = localStorage.getItem
 
-let router = Router();
+const router = Router();
+const cache = new NodeCache(); // Initialize node-cache
 
+
+// router.get('/', (req, res, next) => {
+//     // Check if data exists in cache
+//     const cachedData = cache.get('tasks');
+//     if (cachedData) {
+//         return res.json(cachedData); // Return cached data
+//     }
+
+//     // If data not in cache, fetch from database
+//     fetchTasks(req, res, next).then((tasks) => {
+//         // Store data in cache with a TTL (time-to-live) of 10 seconds
+//         cache.set('tasks', tasks, 10);
+//         res.json(tasks);
+//     }).catch(next);
+// });
 
 // Read all tasks
-// Protected route
-// router.get('/',verify,fetchTasks) // Home page
+router.get('/', verify,(req, res, next) => {
+    // Check if data exists in cache
+    const cachedData = cache.get('tasks');
+    if (cachedData) {
+        return res.json(cachedData); // Return cached data
+    }
 
-router.get('/',fetchTasks);
+    // If data not in cache, fetch from database
+    fetchTasks(req, res, next).then((tasks) => {
+        // Store data in cache with a TTL (time-to-live) of 10 seconds
+        cache.set('tasks', tasks, 10);
+        res.json(tasks);
+    }).catch(next);
+});
 
 // Adding task
-// router.get('/task/addTask',getAddTask);
+router.post('/addTask', verify, (req, res, next) => {
+    // Clear cache when a new task is added
+    cache.del('tasks');
+    postAddTask(req, res, next);
+});
 
-router.post('/addTask',verify, postAddTask);
-
-// Update 
-// router.get('./task/updateTask/',getUpdateTask);
-router.patch('/updateTask/:id',verify,patchUpdateTask)
+// Update task
+router.patch('/updateTask/:id', verify, (req, res, next) => {
+    // Clear cache when a task is updated
+    cache.del('tasks');
+    patchUpdateTask(req, res, next);
+});
 
 // Delete task
-router.delete('/:id',verify,deleteTask)
+router.delete('/:id', verify, (req, res, next) => {
+    // Clear cache when a task is deleted
+    cache.del('tasks');
+    deleteTask(req, res, next);
+});
 
 export default router;
